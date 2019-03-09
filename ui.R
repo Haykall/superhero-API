91d76efe514e64908619af171561b7c7b030ea6f
Haykal Mubin <hmubin00@gmail.com>
2019-03-09 13:34:25 -0800

    Added some scaffold code

--- ui.R
+++ ui.R
@@ -1,1 +1,29 @@
 # UI file
+library(shiny)
+library(plotly)
+
+# Responsbile for the first page with two widgets controling one visualization
+# allowing users to change the race population shown and the opacity
+first_page <- tabPanel(
+  "Welcome",
+  titlePanel("Superhero Data Analysis"),
+  sidebarLayout(
+    sidebarPanel(
+      
+    ),
+    mainPanel(
+      h1("Introduction"),
+      p("We are a group of college students who are big fans of superheros.")
+      )
+  )
+  )
+
+# Responsible for the second page with two widgets controling one visualization
+# allowing users to change the age group in pverty and the color of the points
+
+
+# Describes the loyout of the webpage
+ui <- navbarPage(
+  "Superhero API",
+  first_page
+)
