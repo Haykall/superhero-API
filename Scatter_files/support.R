189af1e4808a41e0433931ebb8dee2f5acbdae81
Jeremy <j1lin0101@gmail.com>
2019-03-13 20:45:40 -0700

    Revert "added comments and close #6"
    This reverts commit 177ca8e2e41fcb24d3f1689021f7c4a387663565.

--- Scatter_files/support.R
+++ Scatter_files/support.R
@@ -1,12 +1,8 @@
 
 library("dplyr")
 
-#calling the csv file with super hero data
 df <- read.csv('Scatter_files/femalechart.csv', stringsAsFactors = FALSE)
 
-#filtering data to show specific power stats
 female <- df %>% select(intelligence, strength, name, race, alignment) 
-#Setting a range for dataframe
 df_range <- range(0:100)
-#Getting the alignments for each hero
 alignment_list <- df %>% select(alignment)
